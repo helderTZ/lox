4b9f9d5cdcae1c0bd10884aabfc57a49bbe88530
Helder Duarte <heldertz2@gmail.com>
2021-01-31 16:44:15 +0000

    Completed challenge 3 from Inheritance chapter.
    Followed Solutions and implemented Array as a native function.

--- jlox/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/com/craftinginterpreters/lox/Interpreter.java
@@ -28,6 +28,23 @@ class Interpreter implements Expr.Visitor<Object>,
       @Override
       public String toString() { return "<native fn>"; }
     });
+
+    globals.define("Array", new LoxCallable() {
+      @Override
+      public int arity() {
+        return 1;
+      }
+    
+      @Override
+      public Object call(Interpreter interpreter,
+                         List<Object> arguments) {
+        int size = (int)(double)arguments.get(0);
+        return new LoxArray(size);
+      }
+
+      @Override
+      public String toString() { return "<array>"; }
+    });
   }
 
   String interpret(Expr expression) {
