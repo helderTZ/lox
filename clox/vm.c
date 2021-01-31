0abfd6d81f38fdda6a1fef868953f53bb4e7c5b8
Helder Duarte <heldertz2@gmail.com>
2021-01-31 23:29:41 +0000

    Completed Virtual Machine
    Completed chapter http://craftinginterpreters.com/a-virtual-machine.html

--- clox/vm.c
+++ clox/vm.c
@@ -4,39 +4,72 @@
 #include "debug.h"
 #include "vm.h"
 
-VM vm; 
+VM vm;
+
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
 
 void initVM() {
+  resetStack();
 }
 
 void freeVM() {
 }
 
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
       case OP_CONSTANT_LONG: {
         Value constant = (uint8_t)READ_CONSTANT() | 
                          (uint8_t)READ_CONSTANT() << 8 |
                          (uint8_t)READ_CONSTANT() << 16;
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NEGATE:   push(-pop()); break;
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
     }
@@ -44,6 +77,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
