72fba7ebe940e0ffbdee37b09bb6d45ccafaebf2
Helder Duarte <heldertz2@gmail.com>
2021-02-13 17:51:40 +0000

    Completed Chapter 24 - Calls and Functions
    Completed http://craftinginterpreters.com/calls-and-functions.html

--- clox/object.c
+++ clox/object.c
@@ -26,6 +26,12 @@ ObjFunction* newFunction() {
   return function;
 }
 
+ObjNative* newNative(NativeFn function) {
+  ObjNative* native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+  native->function = function;
+  return native;
+}
+
 // implements hash algorithm FNV-1a
 uint32_t hashString(const char* key, int length) {
   uint32_t hash = 2166136261u;
@@ -73,6 +79,9 @@ void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
     case OBJ_FUNCTION:
       printFunction(AS_FUNCTION(value));
+      break;
+    case OBJ_NATIVE:
+      printf("<native fn>");
       break;
     case OBJ_STRING:
       printf("'%s'", AS_CSTRING(value));
